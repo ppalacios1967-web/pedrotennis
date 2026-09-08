81f3dcc0c7b50bc7c65e81ee2258dc161452b5d1
ppalacios1967-web <ppalacios1967@gmail.com>
2026-09-07 22:54:58 -0600

    Agregar redirección canónica de www al dominio principal

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.hostname.toLowerCase() === "www.pedrotennis.com") {
+      url.hostname = "pedrotennis.com";
+      return Response.redirect(url.toString(), 301);
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
